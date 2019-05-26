3c9a73f13340075d039ec90f00fa51528b7c6675
georgemoralis <giorgosmrls@gmail.com>
2019-05-27 01:14:20 +0300

    rewrote docastle driver but still doesn't work

--- emulator/src/WIP/mame056/machine/docastle.java
+++ emulator/src/WIP/mame056/machine/docastle.java
@@ -1,88 +1,84 @@
-/***************************************************************************
-
-  machine.c
-
-  Functions to emulate general aspects of the machine (RAM, ROM, interrupts,
-  I/O ports)
-
-***************************************************************************/
-
-/*
- * ported to v0.56
- * using automatic conversion tool v0.01
- */ 
+/**
+ * ported to v056
+ * ported to v0.37b7
+ *
+ */
+/**
+ * Changelog
+ * ---------
+ * 26/05/2019 - rewrote docastle machine to 0.56 (shadow)
+ */
 package WIP.mame056.machine;
 
 import static arcadeflex056.fucPtr.*;
-import static mame056.cpuexecH.*;
+import static arcadeflex036.osdepend.*;
+
 import static mame056.cpuexec.*;
+import static mame056.cpuexecH.*;
 
-// refactor
-import static arcadeflex036.osdepend.logerror;
+public class docastle {
 
-public class docastle
-{
-	
-	
-	
-	static char[] buffer0=new char[9],buffer1=new char[9];
-	
-	
-	
-	public static ReadHandlerPtr docastle_shared0_r  = new ReadHandlerPtr() { public int handler(int offset)
-	{
-		if (offset == 8) logerror("CPU #0 shared0r  clock = %d\n",cpu_gettotalcycles());
-	
-		/* this shouldn't be done, however it's the only way I've found */
-		/* to make dip switches work in Do Run Run. */
-		if (offset == 8)
-		{
-			cpu_cause_interrupt(1,Z80_NMI_INT);
-			cpu_spinuntil_trigger(500);
-		}
-	
-		return buffer0[offset];
-	} };
-	
-	
-	public static ReadHandlerPtr docastle_shared1_r  = new ReadHandlerPtr() { public int handler(int offset)
-	{
-		if (offset == 8) logerror("CPU #1 shared1r  clock = %d\n",cpu_gettotalcycles());
-		return buffer1[offset];
-	} };
-	
-	
-	public static WriteHandlerPtr docastle_shared0_w = new WriteHandlerPtr() {public void handler(int offset, int data)
-	{
-		if (offset == 8) logerror("CPU #1 shared0w %02x %02x %02x %02x %02x %02x %02x %02x %02x clock = %d\n",
-			buffer0[0],buffer0[1],buffer0[2],buffer0[3],buffer0[4],buffer0[5],buffer0[6],buffer0[7],data,cpu_gettotalcycles());
-	
-		buffer0[offset] = (char) data;
-	
-		if (offset == 8)
-			/* awake the master CPU */
-			cpu_trigger.handler(500);
-	} };
-	
-	
-	public static WriteHandlerPtr docastle_shared1_w = new WriteHandlerPtr() {public void handler(int offset, int data)
-	{
-		buffer1[offset] = (char) data;
-	
-		if (offset == 8)
-		{
-			logerror("CPU #0 shared1w %02x %02x %02x %02x %02x %02x %02x %02x %02x clock = %d\n",
-					buffer1[0],buffer1[1],buffer1[2],buffer1[3],buffer1[4],buffer1[5],buffer1[6],buffer1[7],data,cpu_gettotalcycles());
-	
-			/* freeze execution of the master CPU until the slave has used the shared memory */
-			cpu_spinuntil_trigger(500);
-		}
-	} };
-	
-	
-	
-	public static WriteHandlerPtr docastle_nmitrigger_w = new WriteHandlerPtr() {public void handler(int offset, int data)
-	{
-		cpu_cause_interrupt(1,Z80_NMI_INT);
-	} };
+    static /*unsigned*/ char[] u8_buffer0 = new char[9];
+    static /*unsigned*/ char[] u8_buffer1 = new char[9];
+
+    public static ReadHandlerPtr docastle_shared0_r = new ReadHandlerPtr() {
+        public int handler(int offset) {
+            if (offset == 8) {
+                logerror("CPU #0 shared0r  clock = %d\n", cpu_gettotalcycles());
+            }
+
+            /* this shouldn't be done, however it's the only way I've found */
+ /* to make dip switches work in Do Run Run. */
+            if (offset == 8) {
+                cpu_cause_interrupt(1, Z80_NMI_INT);
+                cpu_spinuntil_trigger(500);
+            }
+
+            return u8_buffer0[offset] & 0xFF;
+        }
+    };
+
+    public static ReadHandlerPtr docastle_shared1_r = new ReadHandlerPtr() {
+        public int handler(int offset) {
+            if (offset == 8) {
+                logerror("CPU #1 shared1r  clock = %d\n", cpu_gettotalcycles());
+            }
+            return u8_buffer1[offset] & 0xFF;
+        }
+    };
+
+    public static WriteHandlerPtr docastle_shared0_w = new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+            if (offset == 8) {
+                logerror("CPU #1 shared0w %02x %02x %02x %02x %02x %02x %02x %02x %02x clock = %d\n",
+                        u8_buffer0[0], u8_buffer0[1], u8_buffer0[2], u8_buffer0[3], u8_buffer0[4], u8_buffer0[5], u8_buffer0[6], u8_buffer0[7], data, cpu_gettotalcycles());
+            }
+
+            u8_buffer0[offset] = (char) (data & 0xFF);
+
+            if (offset == 8) /* awake the master CPU */ {
+                cpu_trigger.handler(500);
+            }
+        }
+    };
+
+    public static WriteHandlerPtr docastle_shared1_w = new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+            u8_buffer1[offset] = (char) (data & 0xFF);
+
+            if (offset == 8) {
+                logerror("CPU #0 shared1w %02x %02x %02x %02x %02x %02x %02x %02x %02x clock = %d\n",
+                        u8_buffer1[0], u8_buffer1[1], u8_buffer1[2], u8_buffer1[3], u8_buffer1[4], u8_buffer1[5], u8_buffer1[6], u8_buffer1[7], data, cpu_gettotalcycles());
+
+                /* freeze execution of the master CPU until the slave has used the shared memory */
+                cpu_spinuntil_trigger(500);
+            }
+        }
+    };
+
+    public static WriteHandlerPtr docastle_nmitrigger_w = new WriteHandlerPtr() {
+        public void handler(int offset, int data) {
+            cpu_cause_interrupt(1, Z80_NMI_INT);
+        }
+    };
 }
